5ad8529ffaa1d39f0f2b454e8b797c43b2a391cc
kbc18a11 <wada121400@gmail.com>
2020-12-16 16:40:24 +0900

    add : DBと操作するモデルファイルを作成

---
+++ db/model/Model.js
@@ -0,0 +1,26 @@
+const Nedb = require('nedb-promise');
+
+class Model {
+    /**
+     * @param {string} tableName テーブル名
+     */
+    constructor(tableName) {
+        //テーブル名
+        this.tableName = tableName;
+
+        //テーブル読み込み
+        this.db = new Nedb({ filename: `../dbfile/${tableName}.db`, autoload: true });
+    }
+
+    /**
+     * テーブル名の取得
+     * @returns {string} テーブル名
+     */
+    get getTableName() {
+        return this.tableName;
+    }
+
+    create() {
+
+    }
+}
